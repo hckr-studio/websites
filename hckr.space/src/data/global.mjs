851beefc50cb619683429e2213a8c3d4e86c805d
Aleš Roubíček <ales@roubicek.name>
2026-06-13 06:32:38 +0200

    Added footer to hcke.space

--- hckr.space/src/data/global.mjs
+++ hckr.space/src/data/global.mjs
@@ -8,4 +8,7 @@ export default {
   podcasts: {
     list: ["data-talk", "kanarci-v-siti", "people-ops", "exec", "hra-skolou", "plodne-hovory", "appcast"],
   },
+  get currentYear() {
+    return Temporal.Now.plainDateISO().year;
+  }
 };
